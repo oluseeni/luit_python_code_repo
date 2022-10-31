f5061f141a133d7fa95a58fd3d80931dfac7289f
Seeni Idowu <seeni.idowu@gmail.com>
2022-10-31 23:57:32 +0000

    python work for 10312022

---
+++ Fah_to_Celsius.py
@@ -0,0 +1,7 @@
+# python implementation here
+
+fahrenheit = float(input("What temperature (in Fahrenheit) would you like converted to Celsius? "))
+
+celsius = (fahrenheit - 32) * 5 / 9
+
+print(fahrenheit, "F is", round(celsius, 2), "C")
